9c889312c4745104b32ca8eabc2a83063c6a9449
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-03-14 12:18:14 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,5 @@
 input.onGesture(Gesture.Shake, function () {
     radio.sendNumber(0)
 })
-input.onButtonPressed(Button.A, function () {
-    list = [1, 2, -2]
-    text_list = ["red", "blue", "green"]
-    while (list.length > 0) {
-        radio.sendNumber(list.shift())
-        radio.sendString("" + (text_list.shift()))
-        basic.pause(1000)
-    }
-    radio.sendNumber(0)
-})
-let text_list: string[] = []
-let list: number[] = []
 radio.setGroup(100)
 basic.showArrow(ArrowNames.North)
